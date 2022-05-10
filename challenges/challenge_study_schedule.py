8410816f1858b07098740dfed884d08800d82511
Bruno Pinho <brunopinho@outlook.com>
2022-05-10 19:11:38 -0300

    chore: remove comentários

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,20 +1,10 @@
 def study_schedule(permanence_period, target_time):
     try:
-        # if target_time is None or type(target_time) != int:
-        #     return None
-
         active_students = 0
 
         for student_study_time in permanence_period:
             start_time = student_study_time[0]
             finish_time = student_study_time[1]
-
-            # if (
-            #     type(student_study_time) != tuple
-            #     or type(start_time) != int
-            #     or type(finish_time) != int
-            # ):
-            #     return None
 
             if start_time <= target_time <= finish_time:
                 active_students += 1
