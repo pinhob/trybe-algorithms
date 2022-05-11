61c46effaef261d4ec41726ce4aec2184c5159dc
Bruno Pinho <brunopinho@outlook.com>
2022-05-11 15:35:21 -0300

    feat: faz requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,14 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) <= 1:
+        return False
+
+    for num in nums:
+        if not isinstance(num, int) or num < 1:
+            return False
+
+        num_duplicate_counter = nums.count(num)
+
+        if num_duplicate_counter > 1:
+            return num
+
+    return False
